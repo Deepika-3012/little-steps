4fc32d1c9d7b228fe9fe961be7fd75a7130fb607
Deepika <gnanaveldeeepika@gmail.com>
2026-08-08 19:11:48 +0530

    Fix API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://little-steps-server.onrender.com/api",
+  baseURL: "https://little-steps-server.onrender.com/api",
 });
 
 export default API;
